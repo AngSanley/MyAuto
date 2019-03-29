5d6b794a714dde74a357ff1094f2dae81563c89c
Stanley <angstanley68@gmail.com>
2019-03-29 22:25:27 +0700

    Add back button to RegisterActivity
    Signed-off-by: Stanley <angstanley68@gmail.com>

--- app/src/main/java/binus/mat/ics/myauto/RegisterActivity.java
+++ app/src/main/java/binus/mat/ics/myauto/RegisterActivity.java
@@ -9,5 +9,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
