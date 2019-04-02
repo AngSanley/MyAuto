60539ddd613bdd1e8c01f4f14a1958001a05c354
Andrew Cen <andrew.cen@binus.ac.id>
2019-04-02 10:50:53 +0700

    added timeline views
    Signed-off-by: Andrew Cen <andrew.cen@binus.ac.id>

--- app/src/main/java/binus/mat/ics/myauto/TimelineFragment.java
+++ app/src/main/java/binus/mat/ics/myauto/TimelineFragment.java
@@ -3,13 +3,18 @@ package binus.mat.ics.myauto;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.github.vipulasri.timelineview.TimelineView;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -42,12 +47,90 @@ public class TimelineFragment extends Fragment {
     Gson gson = new Gson();
     OkHttpClient client = new OkHttpClient();
 
+    // Timeline
+    private RecyclerView mRecyclerView;
+    private TimeLineAdapter mAdapter;
+
+    // Temporary data
+    private ArrayList<String> texts;
+
+    public class TimeLineViewHolder extends RecyclerView.ViewHolder {
+        private TimelineView mTimelineView;
+        private TextView mTextView;
+        private String mText;
+
+        public TimeLineViewHolder(View itemView, int viewType) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.text_view);
+            mTimelineView = (TimelineView) itemView.findViewById(R.id.timeline);
+            mTimelineView.initLine(viewType);
+        }
+
+        public void bind(String text) {
+            mText = text;
+            mTextView.setText(mText);
+        }
+    }
+
+    public class TimeLineAdapter extends RecyclerView.Adapter<TimeLineViewHolder> {
+
+        @NonNull
+        @Override
+        public TimeLineViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = View.inflate(getActivity(), R.layout.item_view, null);
+            return new TimeLineViewHolder(view, i);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TimeLineViewHolder timeLineViewHolder, int i) {
+            timeLineViewHolder.bind(texts.get(i));
+        }
+
+        @Override
+        public int getItemCount() {
+            return texts.size();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return TimelineView.getTimeLineViewType(position, getItemCount());
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        texts = new ArrayList<>();
+        for (int i = 1; i <= 20; ++i) {
+            texts.add("Text number " + i);
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_timeline, container, false);
+        View view = inflater.inflate(R.layout.fragment_timeline, container, false);
+
+        mRecyclerView = view.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+
+        return view;
+    }
+
+    private void updateUI() {
+        if (mAdapter == null) {
+            mAdapter = new TimeLineAdapter();
+            mRecyclerView.setAdapter(mAdapter);
+        } else {
+            // enter update data code below
+
+            //.......
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
 
